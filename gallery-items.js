8b96e1ee46dec6db3c201145eacebeca8e0415ff
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-06-12 15:53:49 +0300

    close

--- gallery-items.js
+++ gallery-items.js
@@ -67,27 +67,65 @@ export default [
 import images from "./gallery-items.js";
 
 const galleryBox = document.querySelector(".js-gallery");
-// const overlayBox = document.querySelector(".js-gallery");
-// const lightBox = document.querySelector(".js-lightbox");
-// const lightBoxImg = document.querySelector(".lightbox__image");
-// const closeBtn = document.querySelector(`[data-action="close-lightbox"]`);
+const overlayBox = document.querySelector(".js-gallery");
+const lightBox = document.querySelector(".js-lightbox");
+const lightBoxImg = document.querySelector(".lightbox__image");
+const closeBtn = document.querySelector(`[data-action="close-lightbox"]`);
 
 const imgMarkup = createGallery(images);
 
-
 galleryBox.insertAdjacentHTML("beforeend", imgMarkup);
+
+galleryBox.addEventListener("click", isOpenModal);
+closeBtn.addEventListener("click", isCloseModal);
+
+
 
 function createGallery(images) {
   return images.map(({ preview, original, description }) => {
     return `
     <li class ="gallery__item">
     <a class="gallery__link" href="${original}">
-    <img class="gallery__img" src="${preview}" data-source="${original}" alt="${description}"/>
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
     </a>
     </li>
     `
   }).join("");
 }
 
+function isOpenModal(e) {
+  const galleryEl = e.target;
+  e.preventDefault();
+  if (galleryEl.nodeName !== "IMG") {
+    return;
+  }
 
+  lightBoxImg.src = galleryEl.dataset.source;
+  lightBoxImg.alt = galleryEl.alt;
+  lightBox.classList.add("is-open");
 
+  overlayBox.addEventListener("click", closeModalOverlay)
+  closeBtn.addEventListener("click", isCloseModal);
+  window.addEventListener("keydown", closeEsc);
+}
+
+function isCloseModal(e) {
+  lightBox.classList.remove("is-open")
+  lightBoxImg.src = "";
+  lightBoxImg.alt = "";
+  closeBtn.removeEventListener("click", isCloseModal);
+  overlayBox.removeEventListener("click", closeModalOverlay)
+  window.removeEventListener("keydown", closeEsc);
+}
+
+function closeModalOverlay(e) {
+  if (e.carrentTarget === e.target) {
+    isCloseModal(e);
+  }  
+}
+
+function closeEsc(e) {
+  if (e.code === "Escape") {
+    isCloseModal(e);
+  }  
+}
